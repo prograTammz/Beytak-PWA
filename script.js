e2a13e369a1678108d65637a86801e8f82f16191
Ahmed Tamer Mahmoud <ahmedtamer1998@gmail.com>
2019-03-10 16:33:00 +0200

    change and comments to script.js

--- script.js
+++ script.js
@@ -61,8 +61,11 @@ function fillContWithBigCard(id,data){
             
         };
 }   
-
-
+//toggleDrawer, scrolls the page to the top with 2 approaches one for all
+//browser support and one for IOS safari support,
+//then it will stop the scrolling in the body object.
+//then it will show the naviation drawer and change the menu button to close
+//and vice versa
 function toggleDrawer(event){
     document.body.scrollTop= 0;
     document.documentElement.scrollTop = 0;
@@ -75,23 +78,22 @@ function toggleDrawer(event){
         menuButtonInner.innerHTML = "menu";
     }
 }
-
-
-
-
-// window.addEventListener('hashchange',function(e){
-//     fillContent(window.location.hash.substr(1));
-    
-    
-// })
+//init function that fills the page with home.html when page is loaded
 function init(){
     fillContent("home");
 }
+//route class, usage: new Route(name,htmlname,init).
+//name ex:home,aboutus.
+//htmlName ex: home.html,aboutus.html
+//init: means if it will be run by init function as start route (later on)
 function Route(name,htmlName,init){
     this.name = name;
     this.htmlName = htmlName;
     this.init = init;
 }
+//goToRoute, fills the page with the required path by the name
+//loops over each bottomNavelement and remove active css class
+//if the navigation drawer is open it will toggle it.
 function goToRoute(name){
     fillContent(name);
     var elms = document.getElementsByClassName("bottomNavElm");
@@ -99,8 +101,14 @@ function goToRoute(name){
         elms[i].classList.remove("active");
         
     }
-    toggleDrawer();
+    if(document.getElementById("navigation-drawer").classList[0] == "show-drawer"){
+        toggleDrawer();
+    }
+    
 }
+//it take page name then make a fetch request, after it gets the html
+//file it will turn it to text, then it will await and place the content
+//in the pageContent DOM element
 function fillContent(page){
     fetch(`./views/${page}.html`).then(
         function(res){
