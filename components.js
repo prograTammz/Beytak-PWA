186f3af4c68abba0c3cb94db9cb9c632ab9411fb
Ahmed Tamer Mahmoud <ahmedtamer1998@gmail.com>
2019-03-18 07:05:25 +0200

    Added SearchFilterModal

--- components.js
+++ components.js
@@ -206,14 +206,68 @@ let postCard = `<div class="posts-container col-es-4 col-s-6 col-4">
 </div>
  `;
  let loginModal = `<div class="login-modal-content">
- <h2>LOG IN</h2>
- <form action="" method="post">
-     <label for="login-email">E-mail</label>
-     <input class="modal-input modal-email" type="email" name="login-email" id="login-email" placeholder="Email">
-     <label for="login-password">Password</label>
-     <input class="modal-input modal-password"type="password" name="login-password" id="login-password" placeholder="Password">
-     <p><strong>Invalid Email or Password.</strong> Please check your info is 
-             correctly writtern.</p>
-     <button type="submit" class="modal-submit col-5 col-offset-7">Next</button>
- </form>
-</div>`;
+                    <h2>LOG IN</h2>
+                    <form action="" method="post">
+                        <label for="login-email">E-mail</label>
+                        <input class="modal-input modal-email" type="email" name="login-email" id="login-email" placeholder="Email">
+                        <label for="login-password">Password</label>
+                        <input class="modal-input modal-password"type="password" name="login-password" id="login-password" placeholder="Password">
+                        <p><strong>Invalid Email or Password.</strong> Please check your info is 
+                                correctly writtern.</p>
+                        <button type="submit" class="modal-submit col-5 col-offset-7">Next</button>
+                    </form>
+                    </div>`;
+let installCalculator = `<div class="installment-calculator">
+                        <label class="modal-label" for="deposit-amount">Deposit</label>
+                        <p class="modal-desc">Minimum 10% of the actual price</p>
+                        <input class= "modal-input" type="Text" name="deposit-amount" id="deposit-amount">
+                        <label for="period-duration" class="modal-label">Period(Monthes)</label>
+                        <p class="modal-desc">The seller interest is 2% yearly</p>
+                        <input class="modal-input" type="number" name="period-duration" id="period-duration">
+                        <div class="row">
+                            <div class="col-7">
+                                <p class="payment-desc">Your monthly payment would be: </p>
+                                <p class="payment-amount">122,416 EGP</p>
+                            </div>
+                            <div class="button-cont col-5">
+                                <button class="calc-button">Calculate</button>
+                            </div>
+                        </div>
+                        </div>`;
+let searchFilet = `<div class="search-modal">
+                    <h4>Budget</h4>
+                    <div class="row">
+                        <div class="col-5">
+                            <label for="budget-from" class="modal-label">From</label>
+                            <input type="text" name="budget-from" id="budget-from" class="modal-input">
+                        </div>
+                        <div class="col-offset-2 col-5">
+                                <label for="budget-to" class="modal-label">To</label>
+                                <input type="text" name="budget-to" id="budget-to" class="modal-input">
+                        </div>
+                    </div>
+                    <h4>Bedrooms:</h4>
+                        <div class="row">
+                            <p class="slider-value col-2 col-offset-1">1</p>
+                            <div class="col-6">
+                                <input class="slider" type="range" name="" id="" min="1" max="12">
+                            </div>
+                            <p class="slider-value col-2">12</p>
+                        </div>
+                    <h4>Livingrooms:</h4>
+                        <div class="row">
+                            <p class="slider-value col-2 col-offset-1">1</p>
+                            <div class="col-6">
+                                <input class="slider" type="range" name="" id="" min="1" max="8">
+                            </div>
+                            <p class="slider-value col-2">8</p>
+                        </div>
+                    <select class="landing-select-menu col-6" name="type" id="landing-search-type">
+                        <option value="" selected disabled hidden>Type</option>
+                        <option value="Apartment">Apartment</option>
+                        <option value="Villa">Villa</option>
+                        <option value="Commerical">Commerical</option>
+                        <option value="Chalets">Chalets</option>
+                    </select>
+                    <button class="modal-submit col-offset-9 col-3">Ok</button>
+                    </div>`;
